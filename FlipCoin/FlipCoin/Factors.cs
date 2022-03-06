f851696fcd012f603d42851b5d8fa80016f222f5
Apara1330 <polapara1330@gmail.com>
2022-03-06 12:58:52 +0530

    [Apara]- Factor of number completed

---
+++ FlipCoin/FlipCoin/Factors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    class Factors
+    {
+        public static void toFindFact()
+        {
+            int i, number, fact;//variables
+            Console.WriteLine("Enter the Number");
+            number = int.Parse(Console.ReadLine());
+            fact = 1;
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("\nFactorial of Given Number is: " + fact);
+            Console.ReadLine();
+
+        }
+    }
+}
